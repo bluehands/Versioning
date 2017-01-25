bf6c50f72c00bd603b00b5deb4b0cd53b9b9ee75
Liwen Guo <guo@bluehands.de>
2017-01-25 17:44:44 +0100

    automatically remove duplicate assembly version attributes

---
+++ 03 Code/Source/Bluehands.Versioning/RemoveLines.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Bluehands.Versioning
+{
+    public class RemoveLines : Task
+    {
+        [Required]
+        public string[] Files { get; set; }
+
+        [Required]
+        public string[] Patterns { get; set; }
+
+        public override bool Execute()
+        {
+            var patterns = Patterns.Select(pattern => new Regex(pattern)).ToArray();
+            foreach (var file in Files)
+            {
+                var lines = File.ReadAllLines(file);
+                var remaining = lines.Where(line =>
+                {
+                    foreach (var pattern in patterns)
+                    {
+                        if (pattern.IsMatch(line))
+                        {
+                            Log.LogMessage($"Found line in file \"{file}\" that matched the pattern \"{pattern}\":{Environment.NewLine}{line}");
+                            return false;
+                        }
+                    }
+                    return true;
+                }).ToArray();
+                var removedCount = lines.Length - remaining.Length;
+                if (removedCount > 0)
+                {
+                    File.WriteAllLines(file, remaining);
+                    Log.LogMessage($"Removed {removedCount} lines from file \"{file}\".");
+                }
+            }
+            return true;
+        }
+    }
+}
